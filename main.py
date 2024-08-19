c7f53f7ffb5b0dbf76c4c8c15cb2d0f5fc831b7c
bender <bender@MacBook-Pro-bender.local>
2024-08-19 22:51:03 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,9 +1,54 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Depends
+from typing import Optional
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
-@app.get('/')
-def main_page(name: str):
-    return f'Hello {name}'
+class Book(BaseModel):
+    title: str
+    author: str
+    is_classic: bool
+    pages: int
+
+
+class BookArgs:
+    def __init__(
+            self,
+            title: str,
+            author: Optional[str] = None,
+            is_classic: Optional[bool] = None,
+            pages: Optional[int] = Query(default=None, ge=1)
+    ):
+        self.title = title
+        self.author = author
+        self.is_classic = is_classic
+        self.pages = pages
+
+
+@app.get('/books')
+def get_book(search_args: BookArgs = Depends()) -> Book | dict:
+    books = [
+            Book(
+                title="Lion King",
+                author="Abraham Lincoln",
+                is_classic=True,
+                pages=411),
+            Book(
+                title="Star Wars Return Jedi",
+                author="John Lucas",
+                is_classic=False,
+                pages=1111)
+    ]
+    for book in books:
+        if book.title == search_args.title:
+            return book
+    return {"error": "book not found"}
+
+
+@app.post("/books")
+def add_book(book: Book) -> list[Book]:
+    books_list: list = [book]
+    print(books_list)
+    return books_list
